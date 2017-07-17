9f107cffb4a805335301018908bb3648b986c2d6
mvicariaf <amivicaria@hotmail.es>
2017-07-17 17:52:52 +0200

    strength mal

--- vikings.js
+++ vikings.js
@@ -1,27 +1,35 @@
 var names = ['aaron', 'miguel','pilar', 'cristian', 'andres', 'guillermo', 'elisabet', 'juancarlos', 'reyes', 'alvaro',
 		'gabriel', 'santiago', 'cesar', 'javi', 'almudena', 'aurora'];
-var vikingsArmy = function (name, health, strength){ 
-	this.name = name;
-	this.health = health;
-	this.strength = strength;
-
-
+var vikingsArmy = function (){ 
+	this.name = vikingName();
+	this.health = health();
+	this.strength = strength();
 }
-var salud = function () {
+var health = function () {
   return Math.trunc(Math.random() * 100);
 };
-var fuerza = function (salud) {
+var strength = function (health) {
   var strength = Math.trunc(Math.random() * 100);
-  while (salud < strength){
+  while (health < strength || strength !== 0){
     strength = Math.trunc(Math.random() * 100);
   }
   return strength;
 };
 
 var Saxons = function(){
-  this.salud = salud();
-  this.fuerza = fuerza(this.salud);
+  this.health = health();
+  this.strength = strength(this.health);
 };
 
 var saxon = new Saxons();
+var viking1 = new vikingsArmy();
 console.log(saxon);
+console.log(viking1);
+var randomNumber = function (min, max) {
+  return Math.trunc(Math.random() * (max - min) + min);
+};
+
+var vikingName = function (){
+	var index = randomNumber(0,16);
+	return names[index];
+};

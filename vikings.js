77ea2702ea3e154aecd999437495f76275cbc268
aaronmesupe <aaronmp1990@gmail.com>
2017-07-18 10:05:20 +0200

    Arreglado el problema de duplicidad de codigo. Creamos ejercitos vikingos y sajones

--- vikings.js
+++ vikings.js
@@ -1,41 +1,45 @@
 var names = ['aaron', 'miguel','pilar', 'cristian', 'andres', 'guillermo', 'elisabet', 'juancarlos', 'reyes', 'alvaro',
     'gabriel', 'santiago', 'cesar', 'javi', 'almudena', 'aurora'];
+
 var Vikings = function (){ 
   this.name = vikingName();
-  this.health = health();
-  this.strength = strength(this.health);
-}
-var health = function () {
-  return Math.trunc(Math.random() * 100);
-};
-var strength = function (health) {
-  var strength = Math.trunc(Math.random() * 100);
-  while (health < strength || strength === 0){
-    strength = Math.trunc(Math.random() * 100);
-  }
-  return strength;
+  this.health = randomNumber(10,101);
+  this.strength = randomNumber(10,101);
 };
 
 var Saxons = function(){
-  this.health = health();
-  this.strength = strength(this.health);
+  this.health = randomNumber(10,101);
+  this.strength = randomNumber(10,101);
 };
-
 
 var randomNumber = function (min, max) {
   return Math.trunc(Math.random() * (max - min) + min);
 };
-var vikingFighters = randomNumber(0,1001);
-var saxonFighters = randomNumber(0,1001);
+
+var vikingFighters = randomNumber(1,501);
+var saxonFighters = randomNumber(1,501);
 
 var vikingName = function (){
   var index = randomNumber(0,16);
   return names[index];
 };
-function army(vikingFighters){
+
+var round = randomNumber(5,9);
+
+function army(vikingFighters, saxonFighters){
   var vikingsArmy=[];
-  for (var i=0; i<vikingFighters; i++){
-    vikingsArmy[i]=new Vikings();
-  
+  var saxonsArmy=[];
+  for (var i = 0; i < vikingFighters; i++){
+    vikingsArmy[i] = new Vikings();
   }
+  for (var j = 0; j < saxonFighters; j++){
+    saxonsArmy[j] = new Saxons();
+  }
+  console.log(vikingsArmy);
+  console.log(saxonsArmy);
 }
+console.log('Round number: ' + round);
+console.log('Vikings number: ' + vikingFighters);
+console.log('Saxons number: ' + saxonFighters);
+
+army(vikingFighters, saxonFighters);
